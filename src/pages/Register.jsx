9ff380de32a5a3e7c007fbff082de0caeaf8efcb
junbin-tan <junbin.tan@u.nus.edu>
2023-07-05 23:26:46 +0800

    show password and call by id

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,16 +8,33 @@ import { doc, setDoc } from "firebase/firestore";
 import { useNavigate, Link } from "react-router-dom";
 
 
-
 const Register = () => {
+
+
+  const [passwordShown, setPasswordShown] = useState(false);
+
+  // Password toggle handler
+  const togglePassword = (e) => {
+    e.preventDefault();
+    setPasswordShown(!passwordShown);
+  };
+
+  // for error
   const [err, setErr] = useState(false);
-  
+  // for navigation
+  const navigate = useNavigate();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const displayName = e.target[0].value;
-    const email = e.target[1].value;
-    const password = e.target[2].value;
-    const file = e.target[3].files[0];
+    // const displayName = e.target[0].value;
+    // const email = e.target[1].value;
+    // const password = e.target[2].value;
+    // const file = e.target[3].files[0];
+
+    const displayName = document.getElementById('name').value;
+    const email = document.getElementById('email').value;;
+    const password = document.getElementById('password').value;;
+    const file = document.getElementById('file').value;;
 
     try {
       const res = await createUserWithEmailAndPassword(auth, email, password);
@@ -43,6 +60,9 @@ const Register = () => {
 
             //create empty user chats on firestore
             await setDoc(doc(db, "userChats", res.user.uid), {});
+
+            // go to home page after operation
+            navigate("/");
           } catch (err) {
             console.log(err);
             setErr(true);
@@ -60,11 +80,12 @@ const Register = () => {
         <span className="logo">WhatChat</span>
         <span className="title">Register</span>
         <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="display name" />
-          <input type="email" placeholder="email" />
-          <input type="password" placeholder="password" />
+          <input id="name" type="text" placeholder="display name" />
+          <input id="email" type="email" placeholder="email" />
+          <input id="password" type={passwordShown ? "text" : "password"} placeholder="password" />
+          <button id="passwordOnly" onClick={togglePassword}>Show Password</button>
           <input style={{ display: "none" }} type="file" id="file" />
-          <label htmlFor="file">
+          <label id="file "htmlFor="file">
             <img src={AddAvatar} alt="Add Avatar" />
             <span>Add an avatar</span>
           </label>
