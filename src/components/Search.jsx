6692e677472cc8a2590efbe7bb51e655ba10b39f
junbin-tan <junbin.tan@u.nus.edu>
2023-07-12 23:57:24 +0800

    changes

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -41,42 +41,42 @@ const Search = () => {
   };
 
   const handleSelect = async () => {
-    // check if group exist (chats collection in firestore) or not, if no exist create new one
-    const combinedId = currentUser.uid > user.uid ? currentUser.uid + user.uid : user.uid + currentUser.uid;
-    try {
-      const res = await getDoc(doc(db, "chats", combinedId));
-      if (!res.exists()) {
-        //dont exist create chat in chats collection
-        await setDoc(doc(db, "chats", combinedId), {messages:[]})
-        
-        //create user chats --> record in user1 and user2 userChats record
-        await updateDoc(doc(db, "userChats", currentUser.uid), {
-          [combinedId+".userInfo"]:{
-            uid:user.uid,
-            displayName: user.displayName,
-            photoURL: user.photoURL
-          },
-          [combinedId+".date"]: serverTimestamp()
-        });
+     //check whether the group(chats in firestore) exists, if not create
+     const combinedId =
+     currentUser.uid > user.uid
+       ? currentUser.uid + user.uid
+       : user.uid + currentUser.uid;
+   try {
+     const res = await getDoc(doc(db, "chats", combinedId));
 
-        await updateDoc(doc(db, "userChats", user.uid), {
-          [combinedId+".userInfo"]:{
-            uid:currentUser.uid,
-            displayName: currentUser.displayName,
-            photoURL: currentUser.photoURL
-          },
-          [combinedId+".date"]: serverTimestamp()
-        });
-      }
-    } catch (err) {
-      setErr(true);
-    }
-    // craete user chats for both
+     if (!res.exists()) {
+       //create a chat in chats collection
+       await setDoc(doc(db, "chats", combinedId), { messages: [] });
 
-    setUser(null);
-    setUsername("");
+       //create user chats
+       await updateDoc(doc(db, "userChats", currentUser.uid), {
+         [combinedId + ".userInfo"]: {
+           uid: user.uid,
+           displayName: user.displayName,
+           photoURL: user.photoURL,
+         },
+         [combinedId + ".date"]: serverTimestamp(),
+       });
 
-  };
+       await updateDoc(doc(db, "userChats", user.uid), {
+         [combinedId + ".userInfo"]: {
+           uid: currentUser.uid,
+           displayName: currentUser.displayName,
+           photoURL: currentUser.photoURL,
+         },
+         [combinedId + ".date"]: serverTimestamp(),
+       });
+     }
+   } catch (err) {}
+
+   setUser(null);
+   setUsername("")
+ };
 
   return (
     <div className="search">
